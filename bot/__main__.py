da6a99f40724821902ea2eabfeca1695504e98a4
EX69 <cosadiz69@gmail.com>
2021-11-26 22:07:46 +0800

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,8 +45,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/iamLiquidX/MirLeechX")
-    buttons.buildbutton("Group", "https://t.me/mirrorLeechTelegramBot")
+    buttons.buildbutton("Channel", "https://t.me/art2work")
+    buttons.buildbutton("Movie", "https://t.me/art2work_movie")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
